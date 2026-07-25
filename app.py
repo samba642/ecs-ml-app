2c093b8716ae14dfdc32c22689338ef17f37ec50
samba642 <109035530+samba642@users.noreply.github.com>
2026-07-26 00:25:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,4 +12,4 @@ def predict():
     return jsonify({"prediction": [0.87], "model_version": "v1.0"}), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=5000)
